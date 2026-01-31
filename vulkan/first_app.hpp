4ba463fac13ff54beceae3843434a775aef66f8c
Jack Griffiths Rico <j.griffithsrico@gmail.com>
2026-01-31 18:39:58 +0000

    added swap chain and made lve pipeline a unique pointer

--- vulkan/first_app.hpp
+++ vulkan/first_app.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
+#include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
 
 namespace lve
 {
@@ -10,9 +16,25 @@ namespace lve
       static constexpr int WIDTH = 800;
       static constexpr int HEIGHT = 600;
 
+      FirstApp();
+      ~FirstApp();
+
+      FirstApp(const FirstApp&) = delete;
+      FirstApp& operator=(const FirstApp&) = delete;
+
       void run();
 
     private:
+      void createPipelineLayout();
+      void createPipeline();
+      void createCommandBuffers();
+      void drawFrame();
+
       LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan"};
+      LveDevice lveDevice{lveWindow};
+      LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+      std::unique_ptr<LvePipeline> lvePipeline;
+      VkPipelineLayout pipelineLayout;
+      std::vector<VkCommandBuffer> commandBuffers;
   };
 }
